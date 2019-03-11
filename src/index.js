74bdba84cadf055028c502cb51457f733a14fead
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2019-03-10 23:11:23 -0700

    Starting to port over the site. CSS not right yet.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'react-bootstrap'
-import 'font-awesome'
-// import 'bootstrap/dist/css/bootstrap-theme.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
