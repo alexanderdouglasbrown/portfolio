c6ba65782159be7d534636cf27eb386bd988b39a
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2019-03-21 18:23:51 -0700

    I want to change machines

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -1,9 +1,21 @@
 import React from "react"
+import "./Lightbox.css"
 
 class Lightbox extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showLightbox: false,
+            image: ""
+        }
+    }
+
     render() {
         return (
-            <div></div>
+            <div id="lightbox">
+                <img src="https://via.placeholder.com/450" alt={this.props.alt || ""} />
+            </div>
         )
     }
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -6,14 +6,19 @@ class Project extends React.Component {
     titleWithURL() {
         return (<a href={this.props.url}>{this.props.title}</a>)
     }
+
     titleWithoutURL() {
         return (<span>{this.props.title}</span>)
     }
-    
+
+    openLightbox = () => {
+        console.log("Open lightbox")
+    }
+
     render() {
         return (
             <div className="row project">
-                <div className="col-sm-4 col-xs-12">
+                <div className="col-sm-4 col-xs-12" onClick={this.openLightbox} style={{cursor: "pointer"}}>
                     <img className="img-thumbnail img-responsive project-image" alt="" src={this.props.image} />
                 </div>
                 <div className="col-sm-8 col-xs-12">
