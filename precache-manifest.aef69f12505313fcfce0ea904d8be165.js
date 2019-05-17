70c33774ce5ec63a4e15409e9784172156540926
Alex Brown <alexanderdouglasbrown@gmail.com>
2019-05-16 20:58:55 -0700

    Updates

---
+++ precache-manifest.aef69f12505313fcfce0ea904d8be165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0270e462ed75807ada95",
+    "url": "/static/js/main.8dc65325.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "0270e462ed75807ada95",
+    "url": "/static/css/main.9d5cb947.chunk.css"
+  },
+  {
+    "revision": "65a12863d3d549d6ab00dc83560ea505",
+    "url": "/index.html"
+  }
+];
