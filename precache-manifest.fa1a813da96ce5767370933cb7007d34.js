cc55699c7a0b1151c62b48ea99068220ca144b8c
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2019-05-16 23:23:51 -0700

    Updates

---
+++ precache-manifest.fa1a813da96ce5767370933cb7007d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1fdd4602c4434e113921",
+    "url": "/static/js/main.348296f8.chunk.js"
+  },
+  {
+    "revision": "300c13e89f0e1c70496a",
+    "url": "/static/js/2.6f6b556b.chunk.js"
+  },
+  {
+    "revision": "1fdd4602c4434e113921",
+    "url": "/static/css/main.64e2e8fc.chunk.css"
+  },
+  {
+    "revision": "6c249dc01fbb24e2e8622d47dedf427b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f90b7746c7c206f5f43cdaccf8dd947.js"
+  "/precache-manifest.fa1a813da96ce5767370933cb7007d34.js"
 );
 
 workbox.clientsClaim();
