7c7e127f70fc0d3a60d8ebf7924be4346a5c9cd6
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2019-10-17 21:14:36 -0700

    Updates

---
+++ precache-manifest.414fdaac1796151acaf5a75ed5013009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4a543a12b4878f11431ea46bf0bff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afaf371ed8e2bd9136cb",
+    "url": "/static/css/main.b1ef7503.chunk.css"
+  },
+  {
+    "revision": "8f784ee54855e597ad02",
+    "url": "/static/js/2.a9946ea9.chunk.js"
+  },
+  {
+    "revision": "afaf371ed8e2bd9136cb",
+    "url": "/static/js/main.f193d600.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
